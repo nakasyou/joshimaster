e1f64d23f3e30006129c88a43c8d48ea5c0e1d56
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-11-13 22:00:44 +0900

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
   <div class="text-center my-2">
     <div class="text-2xl">JosiMaster</div>
     <p>マルコフ連鎖のソース: ChatGPT</p>
+    <a href="https://github.com/nakasyou/joshimaster" class="underline hover:no-underline">ソースコードをGitHubで見る</a>
   </div>
   <div class="mx-2">
     <Quiz />
