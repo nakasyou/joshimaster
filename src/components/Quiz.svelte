2b4d81b6c2e126a8aa9cf7d726ba1cd683508017
nakasyou <syoutarou1103@outlook.jp>
2023-11-13 19:07:19 +0900

    unko

--- src/components/Quiz.svelte
+++ src/components/Quiz.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
   import malko_ from '../assets/malko.json'
+  import wordDatas_ from '../assets/words.json'
   const malko: Record<string, string[] | undefined> = malko_
+  const wordDatas: Record<string, {
+    body: string
+    joshi?: string | undefined
+  }> = wordDatas_
+
   const malkoEntries = Object.entries(malko)
   let words: {
+    word: string
+    joshi: string
   }[] = []
-  words.push(...malkoEntries[Math.floor(malkoEntries.length * Math.random())][0].split(' '))
+  words.push(...malkoEntries[Math.floor(malkoEntries.length * Math.random())][0].split(' ').map(entry => {
+    return {
+      word: entry,
+      joshi: wordDatas[entry].joshi || ''
+    }
+  }))
 
   for (let i = 0; i !== 10000; i++) {
     const key = words.slice(-2).join(' ')
@@ -14,9 +27,13 @@
       break
     }
     const nextEntry = nextEntries[Math.floor(Math.random() * nextEntries.length)]
-    words.push(nextEntry)
+    words.push({
+      word: nextEntry,
+      joshi: wordDatas[entry].joshi || ''
+    })
   }
 </script>
 <div>
+  {#for}
   { words.join('') }
 </div>
